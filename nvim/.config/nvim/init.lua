de05025e6490213ec15a3f819f96ce16ae3f6d18
Nick White <nick@nickwhite.co>
2026-01-09 16:24:28 -0800

    Updates

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -32,7 +32,7 @@ vim.g.mapleader = " "
 vim.cmd.colorscheme("retrobox")
 
 local system_theme =
-  vim.system({ "defaults", "read", "-g", "AppleInterfaceStyle" }):wait().stdout:gsub("%s+", "")
+    vim.system({ "defaults", "read", "-g", "AppleInterfaceStyle" }):wait().stdout:gsub("%s+", "")
 
 if system_theme == "Dark" then
   vim.opt.background = "dark"
@@ -112,7 +112,7 @@ require("conform").setup({
     if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
       return
     end
-    return { lsp_format = "fallback" }
+    return { lsp_format = "last" }
   end,
 })
 
@@ -156,5 +156,47 @@ require("lualine").setup({
   },
 })
 
--- nvim-lspconfig
-require("nvim-lspconfig")
+-- lsp
+vim.lsp.enable("kotlin_language_server")
+vim.lsp.enable("lua_ls")
+vim.lsp.enable("rust_analyzer")
+vim.lsp.enable("ts_ls")
+vim.lsp.enable("eslint")
+vim.lsp.enable("sourcekit")
+
+local augroup = vim.api.nvim_create_augroup("lsp", { clear = true })
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = augroup,
+  callback = function(ev)
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<space>f", function()
+      vim.lsp.buf.format({ async = true })
+    end, opts)
+    vim.keymap.set("n", "<space>d", function()
+      vim.diagnostic.open_float({
+        scope = "cursor",
+        focusable = true,
+        close_events = {
+          "CursorMoved",
+          "CursorMovedI",
+          "BufHidden",
+          "InsertCharPre",
+          "WinLeave",
+        },
+      })
+    end)
+  end,
+})
